cf4ab50d775c7e303be41849520976784df7943d
Naym <naym57@hotmail.fr>
2023-10-02 15:55:20 +0200

    header footer création

---
+++ app/public/wp-content/themes/barbershop/footer.php
@@ -0,0 +1,11 @@
+        <footer class="site-footer">
+            <div class="container">
+                
+                    <p>Copyright</p>
+                
+            </div>
+        </footer>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ app/public/wp-content/themes/barbershop/index.php
@@ -0,0 +1,19 @@
+<?php get_header();?>
+
+<div class="container">
+    Container 1
+</div>
+
+<div class="container">
+    Container 2
+</div>
+
+<div class="container">
+    Container 3
+</div>
+
+<div class="container">
+    Container 4
+</div>
+
+<?php get_footer();?>
