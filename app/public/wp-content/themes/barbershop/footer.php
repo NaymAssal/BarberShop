645ea63c9ecff0b7ddb60ac2fb5386ea80f27ab6
Naym <naym57@hotmail.fr>
2023-10-03 08:45:00 +0200

    creation des sections

--- app/public/wp-content/themes/barbershop/footer.php
+++ app/public/wp-content/themes/barbershop/footer.php
@@ -1,4 +1,9 @@
         <footer class="site-footer">
+            <div>
+                <h2>Get Our Free Pounch Cukurib Just For You</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, quod quis iusto doloremque</p>
+                <button>Subscribe Now</button>
+            </div>
             <div class="container">
                 
                     <p>© 2020 Arcstress All rights reserved</p>
---
+++ app/public/wp-content/themes/barbershop/page-home.php
@@ -0,0 +1,56 @@
+<?php get_header();?>
+
+
+
+<div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero")">
+                        <div class="overlay">
+                            <div class="container">
+                                <div class="hero-items">
+                                    <div class="hero">
+                                        <h1>Lorem ipsum</h1>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam veniam ratione nisi minima eaque porro pariatur beatae consequatur voluptatem quo explicabo non, mollitia aperiam nobis ipsum numquam doloribus sapiente illo!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="services">
+                        <div class="service-l">
+                            <h3>- Traitement <br/>- Good price <br/>- Good Quality</h3>
+                        </div>
+                        <div class="service-r">
+                            <h2>We Have Features And Services For Fans Of Cukurin</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque iure quibusdam elige</p>
+                            <button>Explore now</button>
+                        </div>
+                    </section>
+                    <section class="step">
+                        <div class="step-l">
+                            <h2>Step By Step We Cut Hair With Professional Stylist</h2>
+                            <p>- Wash hair<br/>- Cut hair<br/>-Hair coloring</p>
+                        </div>
+                        <div class="step-r">
+                            <p>IMAGE</p>
+                        </div>
+
+                    </section>
+                    <section class="prix">
+                        <div class="prix-u">
+                            <h2>The Best Planning Price For You</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto explicabo ipsa des</p>
+                        </div>
+                        <div class="prix-d">les prix</div>
+                    </section>
+                    <section class="avis">
+                        <h2>What Do They Think For Us</h2>
+                        <p>Avis</p><br/>
+                        <p>Profil   <-  -></p>
+                    </section>
+                </main>
+            </div>
+        </div>
+
+<?php get_footer();?>
